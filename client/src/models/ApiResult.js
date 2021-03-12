c839d96cc4f863c4d55a13c03689577812be13ea
TuongNguyen14 <tuong14041997@gmail.com>
2021-03-12 22:15:15 +0700

    First

--- client/src/models/ApiResult.js
+++
@@ -1,9 +0,0 @@
-class ApiResult {
-    constructor(type, message, data) {
-        this.type = type;
-        this.message = message;
-        this.data = data;
-    }
-}
-
-export default ApiResult;
